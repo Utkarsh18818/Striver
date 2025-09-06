f93b6c69070b5a858f98f71b34da840140858007
Utkarsh <Utkarsh190707@gmail.com>
2025-09-06 18:04:02 +0530

    Array_02

---
+++ Array/problem_02.c
@@ -0,0 +1,13 @@
+// Rotate Array by K places. 
+// Most optimal solution.
+
+void main(){
+    int i;
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    printf("Enter Elements in array:\n");
+    for(i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+
+}
